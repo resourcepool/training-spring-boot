e035cb653fa30079ebe0fccbba5bdc63f5e7bb8f
loic ortola <contact@loicortola.com>
2019-09-09 16:42:41 +0200

    Fixed issue with Id generation

--- src/main/java/fr/takima/demo/User.kt
+++ src/main/java/fr/takima/demo/User.kt
@@ -1,14 +1,14 @@
 package fr.takima.demo
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.Id
+import javax.persistence.*
 
 /**
  *
  */
 @Entity(name = "users")
-data class User(@Id var id: Long?,
+data class User(
+                @GeneratedValue(strategy = GenerationType.IDENTITY)
+                @Id var id: Long?,
                 @Column(name = "first_name") var firstName: String?,
                 @Column(name = "last_name") var lastName: String?,
                 @Column(name = "age") var age: Int?) {
